414946f52672cada7a023e2ffe15cd215b497fdd
Rafael Galdamez <rafaelgaldamez@Rafaels-MacBook-Pro.local>
2017-08-23 21:52:19 -0700

    final

---
+++ server.js
@@ -0,0 +1,35 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var path = require('path');
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use('/', express.static(path.join(__dirname, "/app/public")));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes-C.js")(app);
+require("./app/routes/api-routes-U.js")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
